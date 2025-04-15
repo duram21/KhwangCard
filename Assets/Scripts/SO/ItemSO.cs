2134694215ee205cf284a4b34eb9ce32ad59f6fe
duram21 <cubeenjoy@naver.com>
2025-04-16 02:12:52 +0900

    Initial commit

---
+++ Assets/Scripts/SO/ItemSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+  public string name;
+  public int attack;
+  public int health;
+  public Sprite sprite;
+  public float percent;
+}
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Object/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+  public Item[] items;
+}
