0cee81cf1f48618ea6ddb41e516aacc400dbde9c
duram21 <cubeenjoy@naver.com>
2025-04-16 03:24:48 +0900

    카드 초기 세팅 및 뽑기 구현

---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Order", menuName = "Scriptable Objects/Order")]
+public class Order : MonoBehaviour
+{
+    [SerializeField] Renderer[] backRenderers;
+    [SerializeField] Renderer[] middleRenderers;
+    [SerializeField] string sortingLayerName;
+    int originOrder;
+
+
+  private void Start()
+  {
+    SetOrder(0);
+  }
+
+  public void SetOriginOrder(int originOrder)
+  {
+    this.originOrder = originOrder;
+    SetOrder(originOrder);
+  }
+
+  public void SetMostFrontOrder(bool isMostFront){
+    SetOrder(isMostFront ? 100 : originOrder);
+  }
+  public void SetOrder(int order)
+    {
+      int mulOrder = order * 10;
+
+      foreach (var renderer in backRenderers){
+        renderer.sortingLayerName = sortingLayerName;
+        renderer.sortingOrder = mulOrder;
+      }
+
+      foreach ( var renderer in middleRenderers)
+      {
+        renderer.sortingLayerName = sortingLayerName;
+        renderer.sortingOrder = mulOrder + 1;
+      }
+    }
+
+
+}
