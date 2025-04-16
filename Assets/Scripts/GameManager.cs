ebecf6b0c09bd5ec02b428e4eecc775156657298
duram21 <cubeenjoy@naver.com>
2025-04-16 20:04:27 +0900

    턴 카드 배분 및 턴 알림 UI 구현

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+// 치트, UI, 랭킹, 게임오버
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Inst { get; private set; }
+    void Awake() => Inst = this;
+
+    [SerializeField] NotificationPanel notificationPanel;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        InputCheatKey();
+#endif
+    }
+
+    void InputCheatKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            TurnManager.OnAddCard?.Invoke(true);
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            TurnManager.OnAddCard?.Invoke(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            TurnManager.Inst.EndTurn();
+        }
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(TurnManager.Inst.StartGameCo());
+    }
+
+    public void Notification(string message)
+    {
+        notificationPanel.Show(message);
+    }
+}
